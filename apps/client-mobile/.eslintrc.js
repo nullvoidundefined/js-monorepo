e9648d7f16db2557ea1b677bd230a62a2e8a5579
Ian Greenough <ian@therounds.co>
2025-12-30 21:14:31 +0700

    Sort keys

--- apps/client-mobile/.eslintrc.js
+++ apps/client-mobile/.eslintrc.js
@@ -3,6 +3,19 @@ module.exports = {
   extends: '@react-native',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'react/jsx-sort-props': ['error', {
+      callbacksLast: true,
+      ignoreCase: true,
+      noSortAlphabetically: false,
+      reservedFirst: true,
+      shorthandFirst: true,
+    }],
+    'sort-keys': ['error', 'asc', {
+      caseSensitive: false,
+      natural: true,
+    }],
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
